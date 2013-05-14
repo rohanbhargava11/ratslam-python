c64b7e47a543dd72c6e83ee43eae2fccfd6cd652
Rohan Bhargava <rohan.bhargava11@gmail.com>
2013-05-14 11:07:40 -0300

    working with a camera in ros

---
+++ build/lib.linux-i686-2.7/ratslam/__init__.py
@@ -0,0 +1,6 @@
+from experience_map import *
+from view_templates import *
+from pose_cell_network import *
+from visual_odometer import *
+from simulation import *
+from video import *
--- setup.py
+++ setup.py
@@ -3,23 +3,24 @@
 
 """ distribute- and pip-enabled setup.py for ratslam """
 
-from distribute_setup import use_setuptools
-use_setuptools()
-from setuptools import setup, Extension
-import os, sys
+#from distribute_setup import use_setuptools
+#use_setuptools()
+from setuptools import setup#, Extension
+#import os, sys
 
-import re
+#import re
 
 
 
 setup(
     name='ratslam',
 
+    packages=['ratslam'],
     version='dev',
 
     scripts=['scripts/ratslam'],
 
-    include_package_data=True,
+    #include_package_data=True,
     
     #install_requires=parse_requirements('requirements.txt'),
     #dependency_links=parse_dependency_links('requirements.txt')
